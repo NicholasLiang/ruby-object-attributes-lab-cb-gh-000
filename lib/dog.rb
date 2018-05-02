d344be59c72b7d1090c5d62108f3252abd70ea35
Nicholas Liang <ssss021418@gmail.com>
2018-05-02 09:42:29 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,21 +12,3 @@ class Dog
   end
 end
 
-class Person
- 
-  def initialize(first_name, last_name)
-    @first_name = first_name
-    @last_name = last_name
-  end
- 
-  def name=(full_name)
-    first_name, last_name = full_name.split
-    @first_name = first_name
-    @last_name = last_name
-  end
- 
-  def name
-    "#{@first_name} #{@last_name}".strip
-  end
- 
-end
